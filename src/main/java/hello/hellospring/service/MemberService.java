de2cb5ccd72a06d90b2af1b21e3d65742c823527
JaewooShin <chung_103@naver.com>
2022-08-17 17:36:14 +0900

    20220817 hello-spring by Zs

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,23 +1,24 @@
 package hello.hellospring.service;
 
+import hello.hellospring.controller.MemberController;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Transactional
 public class MemberService {
-    private final MemberRepository memberRepository;
+    private MemberRepository memberRepository;
 
     @Autowired
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
-
 
     /**
      * 회원 가입
